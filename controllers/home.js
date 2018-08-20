b0ade01d72f7f0f713fe6a97c3e560abea77a912
hxm1993 <692705164@qq.com>
2018-08-20 17:25:05 +0800

    添加文章详情页

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,12 @@
 let modules = require("../modules/home");
 var home = async (ctx, next) => {
-    console.log("-------------------------")
-    console.log(ctx.params.page)
     let article;
     let page = ctx.params.page;
     let pageSize = 3;
+    let count = 0;
+    await modules.getCount().then(data => {
+        count = data[0].count;
+    })
     await modules.find(page, pageSize).then((data) => {
         // console.log(data)
         article = data; 
@@ -12,10 +14,41 @@ var home = async (ctx, next) => {
     ctx.body = await ctx.render('home', {
         title: 'home',
         content: article,
-        
+        totalPage: Math.ceil(count / pageSize) ,
+        nowPage: page - 0
     })
 }
 
+var detail = async (ctx, next) => {
+    let index = ctx.params.index;
+    let detailData;
+    let detailComment;
+    await modules.findOnce(index).then((data) => {
+        console.log('detail', data)
+        detailData = data[0];
+    })
+    await modules.getComment(index).then((comment) => {
+        detailComment = comment;
+    })
+    ctx.body = await ctx.render('detail', {
+        detailData: detailData,
+        detailComment: detailComment
+    })
+}
+
+var postComment = async (ctx, next) => {
+    let index = ctx.params.index;
+    console.log("ctx!!!!!!!!!!!!!!!!!!!!!!!!!!!!", ctx.request.body);
+
+    ctx.body = {
+        code: 200,
+        message: '评论成功'
+    };
+    
+}
+
 module.exports = {
-    'GET /home/:page': home
+    'GET /home/:page': home,
+    'GET /detail/:index': detail,
+    'POST /comment/:index': postComment
 }
--- modules/home.js
+++ modules/home.js
@@ -2,12 +2,25 @@ let db = require("../lib/db");
 
 class Home {
     static find(page, pageSize, callback) {
-        console.log(page)
-        let start = (page - 1) * pageSize;
-        
+        let start = (page - 1) * pageSize;  
         let sql = `select * from article limit ${start}, ${pageSize}`;
         console.log(sql)
         return db.query(sql)
+    }
+    static getCount() {
+        let sql = 'select count(id) as count from article';
+        return db.query(sql);
+    }
+    static findOnce(id) {
+        let sql = `select * from article where id = ${id}`;
+        return db.query(sql);
+    }
+    static getComment(id) {
+        let sql = `select * from pinglun as p join admin as a on p.uid = a.id  where aid = ${id}`;
+        return db.query(sql);
+    }
+    static addComment() {
+        let sql = "insert into pinglun value()"
     }
 }
 
